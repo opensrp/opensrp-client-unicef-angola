e0fb5f2069b9ee33c1c7e34686d2a2e443f36a33
Simon Njoroge <snjoroge@ona.io>
2021-06-28 15:04:14 +0300

    Add sync stats view

---
+++ opensrp-unicef-angola/src/main/java/org/smartregister/unicefangola/contract/StatsContract.java
@@ -0,0 +1,12 @@
+package org.smartregister.unicefangola.contract;
+
+public interface StatsContract {
+
+    interface View {
+
+        void showProgressDialog(int titleIdentifier);
+
+        void hideProgressDialog();
+
+    }
+}
