9e945fc7a61ed51e8f6aefe3b2cf3cbb590f38e1
hilpitome <hilpitome@gmail.com>
2021-10-05 06:40:32 +0300

    have stats in its own view #63

---
+++ opensrp-unicef-angola/src/main/java/org/smartregister/unicefangola/activity/CachedStatisticsActivity.java
@@ -0,0 +1,70 @@
+package org.smartregister.unicefangola.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.vijay.jsonwizard.activities.MultiLanguageActivity;
+
+import org.smartregister.child.activity.BaseChildRegisterActivity;
+import org.smartregister.unicefangola.R;
+import org.smartregister.unicefangola.presenter.CachedStatisticPresenter;
+import org.smartregister.view.contract.StatsFragmentContract;
+import org.smartregister.view.fragment.BaseRegisterFragment;
+
+import java.util.Map;
+
+import static org.smartregister.AllConstants.SyncInfo.CACHED_RECURRING_SERVICE_RECORDS;
+import static org.smartregister.AllConstants.SyncInfo.CACHED_VACCINES;
+
+public class CachedStatisticsActivity extends MultiLanguageActivity implements StatsFragmentContract.View {
+
+    private CachedStatisticPresenter presenter;
+    private TextView tvCachedVaccines;
+    private TextView tvCachedRecurrentServiceRecords;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cached_statistics);
+        presenter = new CachedStatisticPresenter(this);
+        initializeViews();
+    }
+
+
+
+    private void initializeViews() {
+        tvCachedVaccines = findViewById(R.id.cached_vaccines);
+        tvCachedRecurrentServiceRecords = findViewById(R.id.cached_recurring_service_records);
+
+
+        Button btnRefreshStats = findViewById(R.id.refresh_button);
+        btnRefreshStats.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                presenter.fetchSyncInfo();
+            }
+        });
+
+        presenter.fetchSyncInfo();
+    }
+
+    @Override
+    public Context getContext() {
+        return null;
+    }
+
+    @Override
+    public void refreshECSyncInfo(Map<String, Integer> syncInfoMap) {
+      tvCachedVaccines.setText(syncInfoMap.get(CACHED_VACCINES)+"");
+      tvCachedRecurrentServiceRecords.setText(syncInfoMap.get(CACHED_RECURRING_SERVICE_RECORDS)+"");
+    }
+
+}
---
+++ opensrp-unicef-angola/src/main/java/org/smartregister/unicefangola/interactor/CachedStatisticsInteractor.java
@@ -0,0 +1,94 @@
+package org.smartregister.unicefangola.interactor;
+
+import net.sqlcipher.Cursor;
+import net.sqlcipher.database.SQLiteDatabase;
+
+import org.smartregister.repository.BaseRepository;
+import org.smartregister.util.AppExecutors;
+import org.smartregister.view.activity.DrishtiApplication;
+import org.smartregister.view.contract.StatsFragmentContract;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import timber.log.Timber;
+
+import static org.smartregister.AllConstants.SyncInfo.CACHED_HEIGHTS;
+import static org.smartregister.AllConstants.SyncInfo.CACHED_RECURRING_SERVICE_RECORDS;
+import static org.smartregister.AllConstants.SyncInfo.CACHED_VACCINES;
+import static org.smartregister.AllConstants.SyncInfo.CACHED_WEIGHTS;
+import static org.smartregister.AllConstants.SyncInfo.UNSYNCED_CLIENTS;
+import static org.smartregister.AllConstants.SyncInfo.UNSYNCED_EVENTS;
+import static org.smartregister.AllConstants.SyncInfo.VALID_CLIENTS;
+import static org.smartregister.AllConstants.SyncInfo.VALID_EVENTS;
+
+public class CachedStatisticsInteractor implements StatsFragmentContract.Interactor {
+
+    private AppExecutors appExecutors;
+
+    private SQLiteDatabase database;
+
+    private StatsFragmentContract.Presenter presenter;
+
+    private Map<String, Integer> syncInfoMap = new HashMap<>();
+
+    public CachedStatisticsInteractor(StatsFragmentContract.Presenter presenter) {
+        this.presenter = presenter;
+        appExecutors = new AppExecutors();
+        database = DrishtiApplication.getInstance().getRepository().getReadableDatabase();
+    }
+
+    @Override
+    public void fetchECSyncInfo() {
+        syncInfoMap = new HashMap<>();
+        syncInfoMap.put(CACHED_VACCINES, 0);
+        syncInfoMap.put(CACHED_RECURRING_SERVICE_RECORDS, 0);
+        syncInfoMap.put(CACHED_HEIGHTS, 0);
+        syncInfoMap.put(CACHED_WEIGHTS, 0);
+
+        String cachedVaccinesSql = "select count(*) vaccines WHERE sync_status != 'Synced'";
+        String cachedRecurringServiceRecordsSql = "select count(*) from recurring_service_records where WHERE sync_status != 'Synced'";
+
+        Cursor cursor = null;
+
+        try {
+
+
+            cursor = database.rawQuery(cachedVaccinesSql, new String[]{});
+            while (cursor.moveToNext()) {
+                populateCachedVaccinesInfo(cursor);
+            }
+            cursor.close();
+
+
+            cursor = database.rawQuery(cachedRecurringServiceRecordsSql, new String[]{});
+            while (cursor.moveToNext()) {
+                populateCachedRecurringServiceRecordInfo(cursor);
+            }
+            cursor.close();
+
+            appExecutors.mainThread().execute(new Runnable() {
+                @Override
+                public void run() {
+                    presenter.onECSyncInfoFetched(syncInfoMap);
+                }
+            });
+        } catch (Exception e) {
+            Timber.e(e);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+
+    private void populateCachedVaccinesInfo(Cursor cursor) {
+        syncInfoMap.put(CACHED_VACCINES, cursor.getInt(0));
+    }
+
+    private void populateCachedRecurringServiceRecordInfo(Cursor cursor) {
+        syncInfoMap.put(CACHED_RECURRING_SERVICE_RECORDS, cursor.getInt(0));
+    }
+}
+
